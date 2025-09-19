c6d1d9e989d41ad8587c7d25f8d4780a2e8965e8
Lokesh Waran <Lokesh.e95@gmail.com>
2025-09-19 22:45:13 +0800

    initial1

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+
+    <div class="container">
+        <h1> My cool Vue App </h1>
+        <button> Add Task</button>
+
+    </div>
+
+</template>
+
+<script>
+
+export default {
+    name: "App"
+}
+
+</script>
+
+<style>
+    #app {
+        text-align: center;
+        color: yellowgreen;
+        margin-top: 60px;
+    }
+
+    .container {
+        border: 1px solid steelblue;
+        padding: 30px;
+        max-width: 500px;
+        margin: auto;
+    }
+
+    button {
+        padding: 10px 20px;
+        font-size: 15px;
+        background: seagreen;
+        color: white;
+        button-radius: 8px;
+        border: none;
+        cursor: pointer;
+    }
+
+</style>
